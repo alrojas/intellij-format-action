2fb6ebce5baba96d46fe7c28d3728495e327f0aa
Alan Rojas <arojas@zagile.com>
2021-11-04 12:20:04 -0500

    Add new input

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,29 +6,30 @@
 # - File include glob pattern.
 # - Whether to fail on file changes.
 
-if [[ $# -ne 3 ]]; then
-  echo 'Exactly three parameters (base dir path, input file pattern, fail on changes) required.'
+if [[ $# -ne 4 ]]; then
+  echo 'Exactly three parameters (base dir path, input file pattern, fail on changes, changed files) required.'
   exit 1
 fi
 
 base_path=$1
 include_pattern=$2
 fail_on_changes=$3
+changed_files=$4
 
 cd "/github/workspace/$base_path" || exit 2
-changed_files_before=$(git status --short)
+formatted_files_before=$(git status --short)
 
-/opt/idea/bin/format.sh -m $include_pattern -r .
+/opt/idea/bin/format.sh -m $include_pattern -r $changed_files
 
-changed_files_after=$(git status --short)
-changed_files=$(diff <(echo "$changed_files_before") <(echo "$changed_files_after"))
-changed_files_count=$(($(echo "$changed_files" | wc --lines) - 1))
+formatted_files_after=$(git status --short)
+formatted_files=$(diff <(echo "$formatted_files_before") <(echo "$formatted_files_after"))
+formatted_files_count=$(($(echo "$formatted_files" | wc --lines) - 1))
 
-echo "$changed_files"
-echo "::set-output name=files-changed::$changed_files_count"
+echo "$formatted_files"
+echo "::set-output name=files-changed::$formatted_files_count"
 
 if [[ "$fail_on_changes" == 'true' ]]; then
-  if [[ $changed_files_count -gt 0 ]]; then
+  if [[ $formatted_files_count -gt 0 ]]; then
     echo 'Failing, because these files changed:'
     exit 1
   fi
